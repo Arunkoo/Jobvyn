2e16d44c490dff20b47a99f9f1c451feae097412
Arun Kumar <pheron408@gmail.com>
2026-01-10 19:10:42 +0530

    updated env of SMTP user/pass

--- backend/utils/src/consumer.ts
+++ backend/utils/src/consumer.ts
@@ -36,8 +36,8 @@ export const startSendMailConsumer = async () => {
             port: 465,
             secure: true,
             auth: {
-              user: process.env.GOOGLE_EMAIL,
-              pass: process.env.GOOGLE_PASSWORD,
+              user: process.env.SMTP_USER,
+              pass: process.env.SMTP_PASSWORD,
             },
           });
 
