40f6eff8702f6c3d79976f25f4eca8aefac0917b
Arun Kumar <pheron408@gmail.com>
2026-01-11 18:04:53 +0530

    integrated routes + controller structure

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.js";
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 5002;
 
+app.use("/api/user", userRoutes);
 app.listen(PORT, () => {
   console.log(`User service is running at http://localhost/${PORT}`);
 });
