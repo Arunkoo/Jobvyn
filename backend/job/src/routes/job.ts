d5a9d8f81155e0cd6eacbb61adf29a3690c7e28c
Arun Kumar <pheron408@gmail.com>
2026-01-18 23:01:55 +0530

    feat: create a new Job controller written + tested

--- backend/job/src/routes/job.ts
+++ backend/job/src/routes/job.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { isAuthenticated } from "../middleware/auth.js";
 import uploadFile from "../middleware/multer.js";
-import { createCompany, deleteCompany } from "../controller/job.js";
+import { createCompany, createJob, deleteCompany } from "../controller/job.js";
 
 const jobRouter = express.Router();
 
 jobRouter.post("/company/new", isAuthenticated, uploadFile, createCompany);
 jobRouter.delete("/company/:companyId", isAuthenticated, deleteCompany);
+jobRouter.post("/new", isAuthenticated, createJob);
 export default jobRouter;
