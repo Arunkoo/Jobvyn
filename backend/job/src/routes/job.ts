986af5dfb69c42d7c52acff28d3b5f082ed1eed1
Arun Kumar <pheron408@gmail.com>
2026-01-19 21:23:05 +0530

    feat: added UPDATE JOB controller + tested {but need to improve later business logic}

--- backend/job/src/routes/job.ts
+++ backend/job/src/routes/job.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import { isAuthenticated } from "../middleware/auth.js";
 import uploadFile from "../middleware/multer.js";
-import { createCompany, createJob, deleteCompany } from "../controller/job.js";
+import {
+  createCompany,
+  createJob,
+  deleteCompany,
+  updateJob,
+} from "../controller/job.js";
 
 const jobRouter = express.Router();
 
 jobRouter.post("/company/new", isAuthenticated, uploadFile, createCompany);
 jobRouter.delete("/company/:companyId", isAuthenticated, deleteCompany);
 jobRouter.post("/new", isAuthenticated, createJob);
+jobRouter.put("/update/:jobId", isAuthenticated, updateJob);
 export default jobRouter;
