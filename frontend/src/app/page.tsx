df7cd2e5eeb54ee245419d9246ec8225bc6eb270
Arun Kumar <pheron408@gmail.com>
2026-01-25 23:26:03 +0530

    feat:Build AI Career Guidance UI component {need some more ui fix..}

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,4 @@
+import CarrerGuidance from "@/components/carrerGuidance";
 import HeroSection from "@/components/hero";
 import React from "react";
 
@@ -5,6 +6,7 @@ const Home = () => {
   return (
     <div>
       <HeroSection />
+      <CarrerGuidance />
     </div>
   );
 };
---
+++ frontend/src/type.ts
@@ -0,0 +1,33 @@
+export interface jobOptions {
+  title: string;
+  responsibilities: string;
+  why: string;
+}
+export interface Skills {
+  title: string;
+  why: string;
+  how: string;
+}
+
+export interface skillsToLearn {
+  category: string;
+  skills: Skills[];
+}
+
+export interface LearningApproach {
+  title: string;
+  points: [];
+}
+export interface SkillCategory {
+  category: string;
+  skills: skillsToLearn[];
+}
+
+export interface CarrerGuideResponse {
+  summary: string;
+  jobOptions: jobOptions[];
+  skillsToLearn: skillsToLearn[];
+  learningApproach: LearningApproach;
+}
+
+export const utils_service_url = "https://localhost:5001";
