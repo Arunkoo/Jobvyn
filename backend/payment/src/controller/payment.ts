762bbd4c9d5e13bed23072cb7fd82de3f180ce0e
Arun Kumar <pheron408@gmail.com>
2026-02-05 22:23:08 +0530

    feat: verifyPayment controller written

--- backend/payment/src/controller/payment.ts
+++ backend/payment/src/controller/payment.ts
@@ -3,7 +3,9 @@ import { AuthenticatedRequest } from "../middleware/auth.js";
 import { sql } from "../utils/db.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import { TryCatch } from "../utils/TryCatch.js";
-
+import crypto from "crypto";
+import dotenv from "dotenv";
+dotenv.config();
 export const CheckOut = TryCatch(
   async (req: AuthenticatedRequest, res, next) => {
     if (!req.user) {
@@ -41,5 +43,39 @@ export const CheckOut = TryCatch(
     res.status(201).json({
       order,
     });
+  },
+);
+
+export const paymentVerification = TryCatch(
+  async (req: AuthenticatedRequest, res, next) => {
+    const user = req.user;
+
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+      req.body;
+
+    const body = razorpay_order_id + "|" + razorpay_payment_id;
+    const expectedSignature = crypto
+      .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET as string)
+      .update(body)
+      .digest("hex");
+
+    const isAuthentic = expectedSignature === razorpay_signature;
+    if (isAuthentic) {
+      const now = new Date();
+      const thirtyDays = 30 * 24 * 60 * 60 * 1000;
+      const expiryDate = new Date(now.getTime() + thirtyDays);
+
+      const [updateUser] =
+        await sql`UPDATE users SET subscription = ${expiryDate} WHERE user_id = ${user?.user_id} RETURNING *`;
+
+      res.json({
+        message: "Subscription Purchased Successfully",
+        updateUser,
+      });
+    } else {
+      return res.status(400).json({
+        message: "paymentFailed",
+      });
+    }
   },
 );
--- backend/payment/src/routes/payment.ts
+++ backend/payment/src/routes/payment.ts
@@ -1,5 +1,10 @@
 import express from "express";
+import { isAuthenticated } from "../middleware/auth.js";
+import { CheckOut, paymentVerification } from "../controller/payment.js";
 
 const router = express.Router();
+
+router.post("/checkout", isAuthenticated, CheckOut);
+router.post("/verifyPayment", isAuthenticated, paymentVerification);
 
 export default router;
