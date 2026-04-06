fc7f17996c905cde3fc0c7a9aa920c2f80d8c3b9
Arun Kumar <pheron408@gmail.com>
2026-04-06 22:47:30 +0530

    refactor(gateway): remove unused express middleware and 404 handler

--- backend/gateway/src/app.ts
+++ backend/gateway/src/app.ts
@@ -9,13 +9,11 @@ import {
   notFoundMiddleware,
 } from "./middlewares/error.middleware.js";
 import { rateLimitMiddleware } from "./middlewares/rateLimit.middleware.js";
+
 const app = express();
 app.set("trust proxy", 1);
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use(requestIdMiddleware);
 app.use(loggerMiddleware);
 app.use(rateLimitMiddleware);
@@ -25,12 +23,5 @@ app.use("/", gatewayRoutes);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
-
-app.use((req, res) => {
-  res.status(404).json({
-    success: false,
-    message: "Route not found",
-  });
-});
 
 export default app;
