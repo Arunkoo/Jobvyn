aa34b008ba9b028c2dc360e37f825ded6d8956e8
Arun Kumar <pheron408@gmail.com>
2026-04-17 22:24:25 +0530

    test(auth): add unit test for registerUser to validate required fields

--- backend/auth/src/test/unit/registerUser.unit.test.ts
+++ backend/auth/src/test/unit/registerUser.unit.test.ts
@@ -26,3 +26,25 @@ import axios from "axios";
 import { registerUser } from "../../controller/auth.js";
 
 import { mockRequest, mockResponse, mockNext } from "../helpers/mockSetup.js";
+
+const data = {
+  id: 1,
+  name: "Arun",
+  email: "someone@example.com",
+  password: "pass123",
+};
+// tests//
+
+describe("registerUser", () => {
+  it("return 400 if required fields are missing", async () => {
+    const req = mockRequest({ name: data.name });
+    const res = mockResponse();
+
+    await registerUser(req as Request, res as Response, mockNext);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      message: "All details are required to register.",
+    });
+    expect(sql).not.toHaveBeenCalled();
+  });
+});
