eaea5678272c87243ddda425d374509f23680060
Arun Kumar <pheron408@gmail.com>
2026-05-11 20:54:39 +0530

    test(auth): add integration test for user registration to verify successful registration for recruiter role

--- backend/auth/src/test/integration/auth.integration.test.ts
+++ backend/auth/src/test/integration/auth.integration.test.ts
@@ -35,4 +35,27 @@ describe("POST /api/auth/register", () => {
     expect(res.status).toBe(409);
     expect(res.body.message).toBe("User with this email already exits");
   });
+  it("return 201 if user registered with role === recruiter", async () => {
+    (sql as unknown as jest.Mock).mockResolvedValueOnce([]);
+    (sql as unknown as jest.Mock).mockResolvedValueOnce([
+      {
+        user_id: 1,
+        name: "Arun",
+        email: "someone@example.com",
+        phone_number: "9999999999",
+        role: "recruiter",
+        created_at: new Date(),
+      },
+    ]);
+    const res = await request(app).post("/api/auth/register").send({
+      name: "Arun",
+      email: "Someone@example.com",
+      password: "999499439",
+      phoneNumber: "9999999999",
+      role: "recruiter",
+    });
+    expect(res.status).toBe(201);
+    expect(res.body.success).toBe(true);
+    expect(res.body.token).toBeDefined();
+  });
 });
