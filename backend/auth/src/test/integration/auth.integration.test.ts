92cab7cbf71b64733fdcc3ce26d27becfe6458fd
Arun Kumar <pheron408@gmail.com>
2026-05-13 21:13:13 +0530

    test(auth): add integration test for jobseeker registration to handle missing buffer scenario

--- backend/auth/src/test/integration/auth.integration.test.ts
+++ backend/auth/src/test/integration/auth.integration.test.ts
@@ -7,12 +7,14 @@ jest.mock("../../producer.js", () => ({
 jest.mock("../../index.js", () => ({
   redisClient: { get: jest.fn(), set: jest.fn(), del: jest.fn() },
 }));
+jest.mock("../../utils/buffer.js", () => jest.fn());
 jest.mock("axios");
 
 // new imports for integration
 import request from "supertest";
 import app from "../../app.js";
 import { sql } from "../../utils/db.js";
+import getBuffer from "../../utils/buffer.js";
 
 describe("POST /api/auth/register", () => {
   it("returns 400 if required fields are missing", async () => {
@@ -71,5 +73,21 @@ describe("POST /api/auth/register", () => {
     expect(res.body.message).toBe(
       "Please upload a valid resume file to authenticate.",
     );
+  });
+  it("return 500 if no buffer is returned by server", async () => {
+    (sql as unknown as jest.Mock).mockResolvedValueOnce([]);
+    (getBuffer as unknown as jest.Mock).mockReturnValueOnce(null);
+
+    const res = await request(app)
+      .post("/api/auth/register")
+      .field("name", "Arun")
+      .field("email", "someone@example.com")
+      .field("password", "999499439")
+      .field("phoneNumber", "9999999999")
+      .field("role", "jobseeker")
+      .attach("file", Buffer.from("fake"), "resume.pdf");
+
+    expect(res.status).toBe(500);
+    expect(res.body.message).toBe("Failed to generate buffer");
   });
 });
